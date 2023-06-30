b3da55640aba7bce4c6995d5f9e12238069699a3
fredericodossantos <frederico.dos.santos@gmail.com>
2023-06-30 16:38:41 -0300

    First commit

---
+++ scene.py
@@ -0,0 +1,71 @@
+from manim import *
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        circle.set_fill(PINK, opacity=0.5)
+        self.play(Create(circle))
+        
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+
+        square = Square()  # create a square
+        square.rotate(PI / 4)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeOut(square))  # fade out animation
+
+class SquareAndCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+
+        square = Square()  # create a square
+        square.set_fill(BLUE, opacity=0.5)  # set the color and transparency
+
+        square.next_to(circle, LEFT, buff=0.5)  # set the position
+        self.play(Create(circle), Create(square))  # show the shapes on screen
+
+class AnimatedSquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        square = Square()  # create a square
+        # create a triangle
+        triangle = Triangle()
+
+        self.play(Create(triangle))  # show the triangle on screen
+        self.play(triangle.animate.shift(LEFT))  # move the triangle to the left
+        self.play(triangle.animate.shift(RIGHT * 2))  # move the triangle to the right
+        self.play(triangle.animate.rotate(PI / 2))  # rotate the triangle 90 degrees
+        self.play(FadeOut(triangle))  # fade out animation
+
+
+
+        self.play(Create(square))  # show the square on screen
+        self.play(square.animate.rotate(PI / 4))  # rotate the square
+        self.play(
+            ReplacementTransform(square, circle)
+        )  # transform the square into a circle
+        self.play(
+            circle.animate.set_fill(PINK, opacity=0.5)
+        )  # color the circle on screen
+
+class DifferentRotations(Scene):
+    def construct(self):
+        left_square = Square(color=BLUE, fill_opacity=0.7).shift(2 * LEFT)
+        right_square = Square(color=GREEN, fill_opacity=0.7).shift(2 * RIGHT)
+        self.play(
+            left_square.animate.rotate(PI), Rotate(right_square, angle=PI), run_time=2
+        )
+        self.wait()
+
+class RotatingSquare(Scene):
+    def construct(self):
+        square = Square().set_fill(BLUE_E, opacity=1)
+        self.play(Create(square))
+        self.play(Rotate(square, PI), run_time=2)
+        self.wait() 
