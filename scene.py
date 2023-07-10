24953ffefc956c937dd1cdf5c96ebdf416847560
fredericodossantos <frederico.dos.santos@gmail.com>
2023-07-10 13:09:52 -0300

    GraphingMovement

--- scene.py
+++ scene.py
@@ -104,15 +104,17 @@ class FittingObjects(Scene):
 
 class GraphingMovement(Scene):
     def construct(self):
-        axes = Axes(x_range=[0,5,1], y_range=[0,3,1], x_length=5, y_length=3,
+        axes = Axes(x_range=[-2,2,1], y_range=[-2,2,1], x_length=4, y_length=4,
                     axis_config={"include_tip": True, "numbers_to_exclude": [0]}).add_coordinates()
         axes.to_edge(UR)
         axis_labels = axes.get_axis_labels(x_label="x", y_label="f(x)")
 
-        graph = axes.plot(lambda x : x**0.5, x_range = [0,4],  color = YELLOW)
+        graph = axes.plot(lambda x : x**2, x_range = [-2,2],  color = YELLOW)
         graphing_stuff = VGroup(axes, graph, axis_labels)
 
         self.play(DrawBorderThenFill(axes),Write(axis_labels))
         self.play(Create(graph))
-        self.play(graphing_stuff.animate.shift(LEFT*3), run_time=3)
+        self.play(graphing_stuff.animate.shift(DOWN*4))
+        self.play(axes.animate.shift(LEFT*3), run_time=3)
         self.wait()
+
