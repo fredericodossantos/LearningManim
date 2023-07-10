4fc8cc0eecb6f69186d6e8ade321946efd8ac5c8
fredericodossantos <frederico.dos.santos@gmail.com>
2023-07-10 14:10:01 -0300

    Graphing

--- scene.py
+++ scene.py
@@ -118,3 +118,25 @@ class GraphingMovement(Scene):
         self.play(axes.animate.shift(LEFT*3), run_time=3)
         self.wait()
 
+class Graphing(Scene):
+    def construct(self):
+
+        my_plane = NumberPlane(x_range=[-6,6], x_length=5, y_range=[-10,10], y_length=5)
+        my_plane.add_coordinates()
+        my_plane.shift(RIGHT*3)
+
+        my_function = my_plane.plot(lambda x : 0.1*(x-5)*x*(x+5), x_range=[-6,6], color=GREEN_B)
+
+        area = my_plane.get_area(my_function, x_range=[-5,5], color=[BLUE, YELLOW])
+        
+        label = MathTex("f(x) = 0.1(x-5)x(x+5)").next_to(my_plane, UP, buff=0.2)
+
+        horizontal_line = Line(start=my_plane.c2p(0,my_function.underlying_function(-2)),
+                               end=my_plane.c2p(2, my_function.underlying_function(-2)), stroke_color = YELLOW, stroke_width =10)
+
+        self.play(Create(my_plane), run_time=2)
+        self.play(Create(my_function), Write(label), run_time=2)
+        self.play(FadeIn(area), run_time=2)
+        
+
+        self.wait(3)
