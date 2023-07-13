195c9b234b1ea8c2f2ad2fad45de6922f3edecec
fredericodossantos <frederico.dos.santos@gmail.com>
2023-07-13 15:15:31 -0300

    correction on Paradox

--- scene4.py
+++ scene4.py
@@ -13,56 +13,54 @@ class Paradox(Scene):
             ).to_edge(DL)
             .set_color(GREY)
         )
-    axes_labes = axes.get_axis_labels(x_label="x", y_label="y")
 
-    func = axes.plot(
-        lambda x: 0.1 * (x - 2) * (x - 5) * (x - 7) + 7, x_range=[0, 10], color=BLUE
-    )
+        axes_labels = axes.get_axis_labels(x_label="x", y_label="y")
 
-    x = ValueTracker(7) # x = 7
-    dx = ValueTracker(2) # dx = 2
-
-    secant = always_redraw(
-        lambda: axes.get_secant_slope_group(
-            x=x.get_value(),
-            graph=func,
-            dx=dx.get_value(),
-            dx_line_color=YELLOW,
-            dy_line_color=ORANGE,
-            dx_label="dx",
-            dy_label="dy",
-            secant_line_color=GREEN,
-            secant_line_length=8,
+        func = axes.plot(
+            lambda x: 0.1 * (x - 2) * (x - 5) * (x - 7) + 7, x_range=[0, 10], color=BLUE
         )
-    )
 
-    dot1 = always_redraw(
-        lambda: Dot()
-        .scale(0.7)
-        .move_to(
-            axes.c2p(x.get_value(),func.underlying_function(x.get_value())))                       
-             
-        )
-    dot2 = always_redraw(
-        lambda: Dot()
-        .scale(0.7)
-        .move_to(
-            axes.c2p(
-                 (x).get_value() + dx.get_value(),
-                 func.underlying_function(x.get_value() + dx.get_value()),
+        x = ValueTracker(7) # x = 7
+        dx = ValueTracker(2) # dx = 2
+
+        secant = always_redraw(
+            lambda: axes.get_secant_slope_group(
+                x=x.get_value(),
+                graph=func,
+                dx=dx.get_value(),
+                dx_line_color=YELLOW,
+                dy_line_color=ORANGE,
+                dx_label="dx",
+                dy_label="dy",
+                secant_line_color=GREEN,
+                secant_line_length=8,
             )
         )
 
-    )
+        dot1 = always_redraw(
+            lambda: Dot()
+            .scale(0.7)
+            .move_to(
+                axes.c2p(x.get_value(),func.underlying_function(x.get_value())))                       
+        )
+        dot2 = always_redraw(
+            lambda: Dot()
+            .scale(0.7)
+            .move_to(
+                axes.c2p(
+                     (x).get_value() + dx.get_value(),
+                     func.underlying_function(x.get_value() + dx.get_value()),
+                )
+            )
+        )
 
-    self.add(axes, axes_labes, func)
-    self.play(Create(VGroup((secant), Create(dot1), Create(dot2))))
-    self.play(dx.animate.set_value(0.001), run_time=8)
-    self.wait(2)
-    self.play(dx.animate.set_value(1), run_time=5)
-    self.wait(2)
-    self.play(dx.animate.set_value(7), run_time=5)
-    self.wait(2)
-    self.play(dx.animate.set_value(2), run_time=5)
-    self.wait(2)
-    
+        self.add(axes, axes_labels, func)
+        self.play(Create(VGroup(secant, dot1, dot2)))
+        self.play(dx.animate.set_value(0.001), run_time=8)
+        self.wait(2)
+        self.play(dx.animate.set_value(1), run_time=5)
+        self.wait(2)
+        self.play(dx.animate.set_value(7), run_time=5)
+        self.wait(2)
+        self.play(dx.animate.set_value(2), run_time=5)
+        self.wait(2)
